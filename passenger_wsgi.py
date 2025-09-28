4944782cc4fb99089555ec6db5c82a4aa01f32f9
Ravi Malani <ravi.malani86@gmail.com>
2025-09-28 14:30:20 +0530

    update passenger files

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,6 +1,5 @@
 import sys, os
-
-# make sure Python can find your project
 sys.path.insert(0, os.path.dirname(__file__))
 
-from app import app as application
+from app import create_app
+application = create_app()
